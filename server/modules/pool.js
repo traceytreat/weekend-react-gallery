4fdaad43b79d9c3862af3833345095dc39669676
Tracey <treatracey@gmail.com>
2022-11-27 13:49:24 -0600

    add database

---
+++ server/modules/pool.js
@@ -0,0 +1,21 @@
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'weekend-gallery',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+})
+
+pool.on('connect', () => {
+    console.log('database connected');
+});
+
+pool.on('error', (error) => {
+    console.log('database connect error', error);
+});
+
+module.exports = pool;
